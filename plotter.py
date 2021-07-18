2c968ac69d4c2aa441f7d128b9795e10a563f526
Andy Weigel <aweigel.ic@gmail.com>
2021-07-18 09:45:26 -0500

    added example function from matplotlib site

--- plotter.py
+++ plotter.py
@@ -46,3 +46,34 @@ slider_position.on_changed(update)
 
 # Display the plot
 plt.show()
+
+# sample function from matplotlib site
+# def my_plotter(ax, data1, data2, param_dict):
+#     """
+#     A helper function to make a graph
+#
+#     Parameters
+#     ----------
+#     ax : Axes
+#         The axes to draw to
+#
+#     data1 : array
+#        The x data
+#
+#     data2 : array
+#        The y data
+#
+#     param_dict : dict
+#        Dictionary of kwargs to pass to ax.plot
+#
+#     Returns
+#     -------
+#     out : list
+#         list of artists added
+#     """
+#     out = ax.plot(data1, data2, **param_dict)
+#     return out
+
+# data1, data2, data3, data4 = np.random.randn(4, 100)
+# fig, ax = plt.subplots(1, 1)
+# my_plotter(ax, data1, data2, {'marker': 'x'})
